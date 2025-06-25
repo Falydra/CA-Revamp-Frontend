78dad8f146730f0dd4931b36a3db782ae07ff281
Falydra <daffa.khususedukasi@gmail.com>
2025-06-25 23:06:56 +0700

    first commit

---
+++ src/lib/axios.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+});
+
+// Automatically fetch CSRF cookie if needed
+axiosClient.interceptors.request.use(async (config) => {
+  await axios.get(`${import.meta.env.VITE_BACKEND_URL}/sanctum/csrf-cookie`, {
+    withCredentials: true,
+  });
+  return config;
+});
+
+export default axiosClient;
