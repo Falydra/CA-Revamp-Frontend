06130d71c9f494299a389a8efbe31cd5300b07ae
Falydra <daffa.khususedukasi@gmail.com>
2025-06-26 04:28:01 +0700

    fix: landing page

---
+++ src/Pages/Welcome.tsx
@@ -0,0 +1,12 @@
+
+import GuestLayout from '@/Layout/GuestLayout';
+import Hero from '@/Components/Hero';
+
+export default function Welcome() {
+    return (
+       <GuestLayout>
+            <Hero />
+       
+       </GuestLayout>
+    )
+}
